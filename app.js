2edd415aaec79334efbeaca3b1a66abcbe0499d8
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-01-23 11:06:47 +0100

    Burger toggle half-way

--- app.js
+++ app.js
@@ -1,21 +1,73 @@
-let dropDown = document.querySelector(".dropdown");
-let followUs = document.querySelector("#follow");
+// let dropDown = document.querySelector(".dropdown");
+// let followUs = document.querySelector("#follow");
 
-followUs.addEventListener("click", () => {
-  dropDown.style.visibility = "visible";
-});
+// followUs.addEventListener("click", () => {
+//   dropDown.style.visibility = "visible";
+// });
 
-let menuBar = document.querySelector("#menu");
-let closeMenu = document.querySelector("#close");
-if (screen.width >= 600) {
-  menuBar.style.display = "block";
-     menuBar.addEventListener("click", () => {
-          // closeMenu.classList.toggle("active");
-    menuBar.style.display = "none";
-       closeMenu.style.display = "block";
-          if (closeMenu.style.display == "block") {
-               
-            menuBar.style.display = "block";
-       }
-  });
-}
+// let menuBar = document.querySelector("#menu");
+// let closeMenu = document.querySelector("#close");
+// if (screen.width >= 600) {
+//   menuBar.onclick
+//   menuBar.style.display = "block";
+//   menuBar.addEventListener("click", () => {
+//     if (menuBar.style.display === "block") {
+//       closeMenu.style.display = "block";
+//       menuBar.style.display = "none";
+//     }
+//   });
+// }
+
+
+let navLinks = document.querySelector("#navlinks");
+let burger = document.querySelector("#burger");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
