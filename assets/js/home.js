b40df52887ba2f5a2996ec747608e61140199082
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-02-14 17:00:50 +0100

    finished the desktop design, completed working with API to get the cryptocurrency values to show, worked on changing the price color to red when the usd_24h_change is less than zero and to green when it is not less than zero, worked on changing the arrow to down and color red when the usd_24h_change is below zero and arrow to up and color green when the usd_24h_change is not below zero

--- assets/js/home.js
+++ assets/js/home.js
@@ -19,18 +19,150 @@ const navSlide = () => {
     burger.classList.toggle("change");
   });
 };
-navSlide();
+// navSlide();
 
-const btc = document.getElementById("bitcoin");
-// var btc = document.getElementById("bitcoin");
-// var btc = document.getElementById("bitcoin");
-// var btc = document.getElementById("bitcoin");
-// var btc = document.getElementById("bitcoin");
+var btc = document.getElementById("bitcoin");
+var eth = document.getElementById("ethereum");
+var bnb = document.getElementById("binance");
+var dogecoin = document.getElementById("dogecoin");
+var litecoin = document.getElementById("litecoin");
+var cardano = document.getElementById("cardano");
+var tether = document.getElementById("tether");
+var solana = document.getElementById("solana");
+var polkadot = document.getElementById("polkadot");
+var xrp = document.getElementById("xrp");
+var down1 = document.getElementById("down1");
+var down2 = document.getElementById("down2");
+var down3 = document.getElementById("down3");
+var down4 = document.getElementById("down4");
+var down5 = document.getElementById("down5");
+var down6 = document.getElementById("down6");
+var down7 = document.getElementById("down7");
+var down8 = document.getElementById("down8");
+var down9 = document.getElementById("down9");
+var down10 = document.getElementById("down10");
 
-let ws = new WebSocket("wss://stream.binance.com:9443/ws/etheur@trade");
-ws.onmessage = (event) => {
-  console.log(event.data);
+var settings = {
+  async: true,
+  scrossDomain: true,
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cbinancecoin%2Clitecoin%2Cdogecoin%2Cethereum%2Csolana%2Ccardano%2Cripple%2Cstellar%2Ctether&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true",
+  method: "GET",
+  header: {},
 };
+$.ajax(settings).done(function (response) {
+  console.log(response)
+  btc.innerHTML = `$${response.bitcoin.usd}`;
+  eth.innerHTML = `$${response.ethereum.usd}`;
+  bnb.innerHTML = `$${response.binancecoin.usd}`;
+  dogecoin.innerHTML = `$${response.dogecoin.usd}`; 
+  litecoin.innerHTML = `$${response.litecoin.usd}`;
+
+  if (response.bitcoin.usd_24h_change < 0) {
+    btc.style.color = "red";
+    down1.style.color = "red";
+  } else {
+    btc.style.color = "green";
+    down1.style.color = "green";
+    down1.classList.remove("bxs-down-arrow");
+    down1.classList.add("bxs-up-arrow");
+  }
+
+  if (response.ethereum.usd_24h_change < 0) {
+    eth.style.color = "red";
+    down2.style.color = "red";
+  } else {
+    eth.style.color = "green";
+    down2.style.color = "green";
+    down2.classList.remove("bxs-down-arrow");
+    down2.classList.add("bxs-up-arrow");
+  }
+
+  if (response.binancecoin.usd_24h_change < 0) {
+    bnb.style.color = "red";
+    down3.style.color = "red";
+  } else {
+    bnb.style.color = "green";
+    down3.style.color = "green";
+    down3.classList.remove("bxs-down-arrow");
+    down3.classList.add("bxs-up-arrow");
+  }
+
+  if (response.dogecoin.usd_24h_change < 0) {
+    dogecoin.style.color = "red";
+    down4.style.color = "red";
+  } else {
+    dogecoin.style.color = "green";
+    down4.style.color = "green";
+    down4.classList.remove("bxs-down-arrow");
+    down4.classList.add("bxs-up-arrow");
+  }
+
+  if (response.litecoin.usd_24h_change < 0) {
+    litecoin.style.color = "red";
+    down5.style.color = "red";
+  } else {
+    litecoin.style.color = "green";
+    down5.style.color = "green";
+    down5.classList.remove("bxs-down-arrow");
+    down5.classList.add("bxs-up-arrow");
+  }
 
 
-btc.innerHTML = "Hello world"
+  
+  cardano.innerHTML = `$${response.cardano.usd}`;
+  tether.innerHTML = `$${response.tether.usd}`;
+  solana.innerHTML = `$${response.solana.usd}`;
+  polkadot.innerHTML = `$${response.stellar.usd}`;
+  xrp.innerHTML = `$${response.ripple.usd}`;
+
+  if (response.cardano.usd_24h_change < 0) {
+    cardano.style.color = "red";
+    down6.style.color = "red";
+  } else {
+    cardano.style.color = "green";
+    down6.style.color = "green";
+    down6.classList.remove("bxs-down-arrow");
+    down6.classList.add("bxs-up-arrow");
+  }
+
+  if (response.tether.usd_24h_change < 0) {
+    tether.style.color = "red";
+    down7.style.color = "red";
+  } else {
+    tether.style.color = "green";
+    down7.style.color = "green";
+    down7.classList.remove("bxs-down-arrow");
+    down7.classList.add("bxs-up-arrow");
+  }
+
+  if (response.solana.usd_24h_change < 0) {
+    solana.style.color = "red";
+    down8.style.color = "red";
+  } else {
+    solana.style.color = "green";
+    down8.style.color = "green";
+    down8.classList.remove("bxs-down-arrow");
+    down8.classList.add("bxs-up-arrow");
+  }
+
+  if (response.stellar.usd_24h_change < 0) {
+    polkadot.style.color = "red";
+    down9.style.color = "red";
+  } else {
+    polkadot.style.color = "green";
+    down9.style.color = "green";
+    down9.classList.remove("bxs-down-arrow");
+    down9.classList.add("bxs-up-arrow");
+  }
+
+  if (response.ripple.usd_24h_change < 0) {
+    xrp.style.color = "red";
+    down10.style.color = "red";
+  } else {
+    xrp.style.color = "green";
+    down10.style.color = "green";
+    down10.classList.remove("bxs-down-arrow");
+    down10.classList.add("bxs-up-arrow");
+  }
+
+})
